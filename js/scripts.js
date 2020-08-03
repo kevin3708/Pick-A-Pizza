e7e8bad8778abf8a6fb68e330c940290dffcc560
Kevin <kevinambundo3@gmail.com>
2020-08-03 12:49:52 +0300

    Add changes to html filr

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
-var hawaiian, boerewors, bbq, periPeri, chickenTikka, vegTikka
+var flavor1, flavor2, flavor3, flavor4, flavor5, flavor6 
 function getInput(){
-    hawaiian=document.getElementById("hawaiian").checked
-    boerewors=document.getElementById("boerewors").checked
-    bbq=document.getElementById("bbq").checked
-    periPeri=document.getElementById("peri-peri").checked
-    chickenTikka=document.getElementById("chicken-tikka").checked
-    vegTikka=document.getElementById("veg-tikka").checked
+    flavor1=(document.getElementsById("flavour1").value)
+    flavor2=(document.getElementsById("flavour2").value)
+    flavor3=(document.getElementsById("flavour3").value)
+    flavor4=(document.getElementsById("flavour4").value)
+    flavor5=(document.getElementsById("flavour5").value)
+    flavor6=(document.getElementsById("flavour6").value)
 }
-function Totals(crust, size){
-    this.crust=document.getElementsByClassName("crustCheckbox")
-    this.size=document.getElementsByClassName("sizeCheckbox")
+console.log(flavor1)
+var crispy, stuffed, glutenFree
+function getInput(){
+    crispy=document.getElementsByName("crust1").checked
+    stuffed=document.getElementsByName("crust2").checked
+    glutenFree=document.getElementsByName("crust3").checked
 }
-console.log(Totals)
+var small, medium, large
+function getInput(){
+    small=document.getElementsByName("size1").checked
+    medium=document.getElementsByName("size2").checked
+    large=document.getElementsByName("size3").checked
+}
+
+
+//function Totals(crust, size){
+  //  this.crust=document.getElementsByClassName("crustCheckbox")
+    //this.size=document.getElementsByClassName("sizeCheckbox")
+//}
+//if ($('#check_id').is(":checked"))
 
 
