2186198a8a98f142f2803e0de6623f1d2831c910
Kevin <kevinambundo3@gmail.com>
2020-08-07 09:12:44 +0300

    Add changes to html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-var flavor1, flavor2, flavor3, flavor4, flavor5, flavor6 
-function getInput(){
-    flavor1=(document.getElementsById("flavour1").value)
-    flavor2=(document.getElementsById("flavour2").value)
-    flavor3=(document.getElementsById("flavour3").value)
-    flavor4=(document.getElementsById("flavour4").value)
-    flavor5=(document.getElementsById("flavour5").value)
-    flavor6=(document.getElementsById("flavour6").value)
+var flavor
+if(document.getElementById("hawaiian").is(":checked")){
+    flavor="Hawaiian"
+}else if(document.getElementById("boerewors").is(":checked")){
+    flavor="Boerewors"
+}else if(document.getElementById("bbq").is(":checked")){
+    flavor="BBQ"
+}else if(document.getElementById("peri-peri").is(":checked")){
+    flavor="PeriPeri"
+}else if(document.getElementById("chickentikka").is(":checked")){
+    flavor="ChickenTikka"
+}else if(document.getElementById("vegtikka").is(":checked")){
+    flavor="VegTikka"
 }
-console.log(flavor1)
+var pizza=flavor
+console.log(pizza)
+
+
 var crispy, stuffed, glutenFree
-function getInput(){
+var crust=function getInput(){
     crispy=document.getElementsByName("crust1").checked
     stuffed=document.getElementsByName("crust2").checked
     glutenFree=document.getElementsByName("crust3").checked
@@ -20,12 +28,5 @@ function getInput(){
     medium=document.getElementsByName("size2").checked
     large=document.getElementsByName("size3").checked
 }
-
-
-//function Totals(crust, size){
-  //  this.crust=document.getElementsByClassName("crustCheckbox")
-    //this.size=document.getElementsByClassName("sizeCheckbox")
-//}
-//if ($('#check_id').is(":checked"))
 
 
